7217774b90f4400d2e6dd54036cfa606738df327
MacBook <macbook@MacBook-Pro.local>
2025-07-08 23:20:58 +0530

    Initial commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <!-- pages/index.vue -->
 <template>
     <div>
-      <h1>Welcome to my Airtable app</h1>
-      <NuxtLink to="/loans">Go to Loans</NuxtLink>
+      <h1>My Airtable App</h1>
+      <NuxtLink to="/loans">Go to Conditions Clearing Base</NuxtLink>
     </div>
   </template>
   
