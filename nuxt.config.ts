19a0d02f02fd3f3734ccd0b38d8955d88222cc8d
MacBook <macbook@MacBook-Pro.local>
2025-07-08 23:51:19 +0530

    Initial project with Vue and API integration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,13 @@
 export default defineNuxtConfig({
-  modules: ['@pinia/nuxt'], 
+  modules: ['@pinia/nuxt'],
   runtimeConfig: {
     AIRTABLE_API_KEY: process.env.AIRTABLE_API_KEY,
     AIRTABLE_BASE_ID: process.env.AIRTABLE_BASE_ID,
   },
   css: [
-    'primevue/resources/themes/aura-light-blue/theme.css',
+    '@/assets/scss/primevue-theme.scss',
+    'primevue/resources/primevue.min.css',
     'primeicons/primeicons.css',
     'primeflex/primeflex.css',
-  ],
-  build: {
-    transpile: ['primevue']
-  }
+  ]
 })
