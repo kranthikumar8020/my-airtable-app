48792cccab16bedc4f318ed24545b0218a874165
MacBook <macbook@MacBook-Pro.local>
2025-07-09 03:47:50 +0530

    new

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,21 @@
-import { defineNuxtConfig } from "nuxt/config";
+import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
-  modules: ['@pinia/nuxt'],
+  modules: [
+    '@pinia/nuxt',
+    ['@nuxtjs/google-fonts', {
+      families: {
+        Poppins: [400, 500, 600, 700]
+      },
+      display: 'swap'
+    }]
+  ],
   runtimeConfig: {
     AIRTABLE_API_KEY: process.env.AIRTABLE_API_KEY,
     AIRTABLE_BASE_ID: process.env.AIRTABLE_BASE_ID,
   },
   css: [
-    'primeicons/primeicons.css', // import icons globally
+    '@/assets/css/main.css'
   ],
   build: {
     transpile: ['primevue']
--- plugins/primevue.ts
+++ plugins/primevue.ts
@@ -3,6 +3,7 @@ import PrimeVue from 'primevue/config'
 import DataTable from 'primevue/datatable'
 import Column from 'primevue/column'
 import Button from 'primevue/button'
+import 'primevue/resources/themes/aura-light-blue/theme.css' // <- change theme file here
 
 export default defineNuxtPlugin(nuxtApp => {
   nuxtApp.vueApp.use(PrimeVue)
