ffb1fc7d4871bb5d8ec99b46da735ea9f8f991a8
MacBook <macbook@MacBook-Pro.local>
2025-07-09 19:03:30 +0530

    new

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,11 @@
 <!-- pages/index.vue -->
 <template>
-    <div>
+  <div>
   <h1>My Airtable App</h1>
   <div><NuxtLink to="/loans">Go to Conditions Clearing Base</NuxtLink></div>
-  <div><NuxtLink to="/Loan-Pipeline">Go to Loan Pipeline View</NuxtLink></div>
-    </div>
+  <div><NuxtLink to="/Loan-Pipeline">Go to Loan Pipeline</NuxtLink></div>
+  <div><NuxtLink to="/Deal-Conditons">Go to Deal Conditons</NuxtLink></div>
+  <div><NuxtLink to="/Response-Submissions">Go to Response Submissions</NuxtLink></div>
+  </div>
   </template>
   
